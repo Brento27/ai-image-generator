d4eaf54c193a7316f52aa7022327a662f9e92ad2
Brent Lombaard <lombaardbrent@gmail.com>
2023-01-29 21:50:55 +0200

    .

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-require('dotenv').config();
 const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
 
 const router = express.Router();
 
