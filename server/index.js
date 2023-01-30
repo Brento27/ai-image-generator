24e6ef4ce24a5f3da1d7d52ea895e9826a46fd50
Brent Lombaard <lombaardbrent@gmail.com>
2023-01-30 08:24:00 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRouter);
-app.use('/api/v1/dalle', dalleRouter);
+//app.use('/api/v1/dalle', dalleRouter);
 
 app.get('/', async (req, res) => {
   res.status(200).json({
