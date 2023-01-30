06dbb246c977ad0f831c2233a4b867b291712ef1
Brent Lombaard <lombaardbrent@gmail.com>
2023-01-30 08:24:59 +0200

    .

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -8,7 +8,7 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration);
+const openAI = new OpenAIApi(configuration);
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello from DALL-E!' });
@@ -18,7 +18,7 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
+    const aiResponse = await openAI.createImage({
       prompt,
       n: 1,
       size: '1024x1024',
